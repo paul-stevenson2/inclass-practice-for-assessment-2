8e69c257630afa7f524d6e26ae507b836b299393
paulstevenson <p.stevenson3000@gmail.com>
2023-05-16 10:02:44 +1200

    sample function array

---
+++ src/arrays.js
@@ -0,0 +1,19 @@
+//TODO - Make a function that sorts and array and deletes any duplicates in the data you have stored.
+
+//NOTE - example: duplicateArray([1,2,3, 4,5]) -> [1,2,3,4,5]
+
+function duplicateArray(arr) {
+    const newArray = [] // This is the empty array that will be filled.
+
+    let currentArray = arr[0] // This is the variable for iterating.
+
+    newArray.push(currentArray) // Here we begin iterating.
+
+    for(let i = 0; i < arr.length; i++) {  // for loop with adding incriment.
+        if(arr[i] !== currentArray) {  // If statement
+            currentArray = [i]
+            newArray.push(currentArray) //NOTE - The push() method adds the specified elements to the end of an array and returns the new length of the array.
+        }
+    }
+    return newArray
+}
